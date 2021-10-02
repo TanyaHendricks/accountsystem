ff840d57b2a003a70bc85a9c3d2a832a25ed962e
TanyaHendricks <bob4robert@gmail.com>
2021-10-02 10:36:39 +0200

    fixed: port + swagger config issues

--- AccountSystem/AccountSystemWebSpringBoot/src/main/java/za.ac.nwu.web.sb/RestServiceApplication.java
+++ AccountSystem/AccountSystemWebSpringBoot/src/main/java/za.ac.nwu.web.sb/RestServiceApplication.java
@@ -2,9 +2,11 @@ package za.ac.nwu.web.sb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class RestServiceApplication {
 
     public static void main(String[] args) {
