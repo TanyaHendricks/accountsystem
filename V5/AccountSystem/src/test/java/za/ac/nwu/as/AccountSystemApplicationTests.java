ae18f3de91e08e589100738db925a2e897e43e74
TanyaHendricks <bob4robert@gmail.com>
2021-10-05 20:11:42 +0200

    refactored: branch v5

---
+++ V5/AccountSystem/src/test/java/za/ac/nwu/as/AccountSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.nwu.as;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
