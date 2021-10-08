5c82b0722d5312f5a81f067ca015184e0ad8b0e4
TanyaHendricks <bob4robert@gmail.com>
2021-10-08 20:54:51 +0200

    redo: module layers

---
+++ V5/AccountSystem/AccountSystemWebSpringBoot/src/test/java/za/ac/nwu/as/AccountSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package za.ac.nwu.as;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountSystemApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}
